4065df3a43151dd288399cb994fbebdaccdc1e14
KshamaAgrawal <kshama@securitytroops.com>
2020-07-28 11:32:35 +0530

    in addform suheading is coming accord to heading

--- Forfirst.js
+++ Forfirst.js
@@ -1,27 +1,37 @@
 $(document).ready(function()
 {
-  $('#btnforHeading').on('click',function(event)
-  {
+  $('.btnforHeading').on('click',function(event){
     var ForHEADING = $("#exampleModal").find('input').val();
-    $('main').append("<section><h1>"+ForHEADING+"</h1></section>");  // showing heading on screen
+    $('main').append("<section><h1>"+ForHEADING+"</h1></section>");         // showing heading on screen
     
-    $(".SltDrp option").remove();
+    $(".SltDrp option").remove();                                           // removing select option(select heading)
     $(".SltDrp").append("<option>select Heading</option>");   
 
-    $("section h1").each(function(key)
-    {
-      var str = $(this).text();
+    $("main section h1").each(function(key){                                // loop on heading 
+      var str = $(this).text();                                            // finding heading text
       key= key+1;
-      $(".SltDrp").append('<option value="'+key+'">'+str+'</option>');
+      $(".SltDrp").append('<option value="'+key+'">'+str+'</option>');    //appending key to heading select
     });
-});
+  });
 
-  $('#btnforSubheading').on('click',function(event)
+  $('.btnforSubheading').on('click',function(event)
   {
-    var forsubhead = $("#Subhead").val();
-    var Sbhead = $(".SltDrp").val();
-    console.log(Sbhead);
-    $('main section:nth-child('+Sbhead+')').append('<h2>'+forsubhead+'</h2>'); 
+    var forsubhead = $(".Subhead").val();                                            // finding subheading input value
+    var Sbhead = $(".SltDrp").val();                                                   // finding select value like 1,2,3,4...
+    $('main section:nth-child('+Sbhead+')').append('<article><h2>'+forsubhead+'</h2></article>');  // appending subhead accord to head
+  }) 
+    $(".SltDrp").change(function(){
+      var forform = $(this).val();                                     // storing heading option value in forform variable
+
+      $(".ForADD option").remove();                                    // removing subheading option (select suheading)
+      $(".ForADD").append("<option>select SubHeading</option>")  
+  
+
+     $('main section:nth-child('+forform+') article h2').each(function(key){  //loop on heading option value
+      var foradd = $(this).text();                                            // assign subheading text in foradd variable
+      key=key+1;
+      $(".ForADD").append('<option value="'+key+'">'+foradd+'</option>');     // appending subheading text in addform button subheading dropdown
+     }) 
   });
 });
 
