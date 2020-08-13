da34e69d04d02ebc5485e31e5cdc6e2b3adb9f1b
KshamaAgrawal <kshama@securitytroops.com>
2020-08-13 17:24:35 +0530

    working on submit enter in add_form

--- Forfirst.js
+++ Forfirst.js
@@ -109,119 +109,121 @@ $(document).ready(function()
   });
 
 
-  $('.btnforaddForm').on("click",function(event){
-    // event.preventDefault()                                 // it will hold data if you press enter kry data will show you and if you don't put this then data will disable when you press enter key
-    var gethead     = $(".head_in_form").val();               // getting heading value in add_form 
-    var getsubhead  = parseInt($(".ForADD").val());           // getting subheading value in add_form
-    var input_type  = $(".form_input").val();                 // getting input type value in add_form
-    var getlabel    = $(".lvl_input").val();                  // getting level value in add_form
-    var getname     = $(".nm_input").val();                   // getting name value in add_form
-    var getplc_hldr = $(".plc_input").val();                  // getting placeholder value in add_form  
-    var getclass    = $(".cls_input").val();                  // getting class value in add_form
-    var getvalue    = $(".val_input").val();                  // getting value of value in add_form
-    var getoptns    = $(".opn_input").val();                  // getting option value in add_form
-    getsubhead=getsubhead+1
-    console.log(getoptns);
-    if(input_type == 'textarea')                                   // condition for textarea if we select textarea in input type dropdown then it will show textarea
-    {
-      $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><label>'+getlabel+'</label><textarea rows="5" cols="30" name="'+getname+'" placeholder="'+getplc_hldr+'" class="'+getclass+'" value="'+getvalue+'"></textarea><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>');   //and it will show textarea if condition is true and also removing the textarea with cross btn 
+  $('.btnforaddForm').on("click",function(event)
+  // $('.FormSubHead').submit(function(event);                                                                                                                                                                                                                                                    $('.FormAddform').submit(function(event)
+  {
+  event.preventDefault()                                 // it will hold data if you press enter kry data will show you and if you don't put this then data will disable when you press enter key
+  var gethead     = $(".head_in_form").val();               // getting heading value in add_form 
+  var getsubhead  = parseInt($(".ForADD").val());           // getting subheading value in add_form
+  var input_type  = $(".form_input").val();                 // getting input type value in add_form
+  var getlabel    = $(".lvl_input").val();                  // getting level value in add_form
+  var getname     = $(".nm_input").val();                   // getting name value in add_form
+  var getplc_hldr = $(".plc_input").val();                  // getting placeholder value in add_form  
+  var getclass    = $(".cls_input").val();                  // getting class value in add_form
+  var getvalue    = $(".val_input").val();                  // getting value of value in add_form
+  var getoptns    = $(".opn_input").val();                  // getting option value in add_form
+  getsubhead=getsubhead+1
+  console.log(getoptns);
+  if(input_type == 'textarea')                                   // condition for textarea if we select textarea in input type dropdown then it will show textarea
+  {
+    $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><label>'+getlabel+'</label><textarea rows="5" cols="30" name="'+getname+'" placeholder="'+getplc_hldr+'" class="'+getclass+'" value="'+getvalue+'"></textarea><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>');   //and it will show textarea if condition is true and also removing the textarea with cross btn 
       
-      formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'textarea', 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue});          // push textarea input in form array
-      console.log(formArray);
-    }
+    formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'textarea', 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue});          // push textarea input in form array
+    console.log(formArray);
+  }
 
-    else if(input_type == 'checkbox')
-    {
-      var forchkbox = getoptns.split(',');
+  else if(input_type == 'checkbox')
+  {
+    var forchkbox = getoptns.split(',');
       
-      $(forchkbox).each(function(key, value){
-        $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><label>'+value+'</label><input type="'+input_type+'" name="'+value+'" class="'+value+'" value = "'+value+'"><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>'); //and it will show checkbox if condition is true and also removing the checkbox with cross btn 
-      })
-      formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'checkbox', 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue, 'option' : forchkbox});           // push checkbox input in form array
-      console.log(formArray);
-    }
-    else if(input_type == 'radio')
-    {
-      var forrdbtn = getoptns.split(',');
+    $(forchkbox).each(function(key, value){
+      $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><label>'+value+'</label><input type="'+input_type+'" name="'+value+'" class="'+value+'" value = "'+value+'"><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>'); //and it will show checkbox if condition is true and also removing the checkbox with cross btn 
+    })
+    formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'checkbox', 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue, 'option' : forchkbox});           // push checkbox input in form array
+    console.log(formArray);
+  }
+  else if(input_type == 'radio')
+  {
+    var forrdbtn = getoptns.split(',');
 
-      $(forrdbtn).each(function(key){
-        $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><label>'+forrdbtn[key]+'</label><input type="'+input_type+'" name="'+getname+'" class="'+getclass+'" value = "'+getvalue+'"><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>'); //and it will show radio if condition is true and also removing the radio with cross btn 
-      })
-      formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'radio', 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue, 'option' : forrdbtn});              // push radio input in form array
-      console.log(formArray);
-    }
-    else if(input_type == 'select')
-    {
-      var forselt = getoptns.split(',');
-      console.log(forselt);
-      var itsForP = $('<p><label>'+getlabel+'</label></p>');
+    $(forrdbtn).each(function(key){
+      $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><label>'+forrdbtn[key]+'</label><input type="'+input_type+'" name="'+getname+'" class="'+getclass+'" value = "'+getvalue+'"><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>'); //and it will show radio if condition is true and also removing the radio with cross btn 
+    })
+    formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'radio', 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue, 'option' : forrdbtn});              // push radio input in form array
+    console.log(formArray);
+  }
+  else if(input_type == 'select')
+  {
+    var forselt = getoptns.split(',');
+    console.log(forselt);
+    var itsForP = $('<p><label>'+getlabel+'</label></p>');
       
-      var forSelect = $('<select><option>select</option>').appendTo(itsForP);
+    var forSelect = $('<select><option>select</option>').appendTo(itsForP);
      
-      for(var i = 0; i < forselt.length; i++){
-        $('<option value="'+forselt[i]+'">'+forselt[i]+'</option>').appendTo(forSelect);
+    for(var i = 0; i < forselt.length; i++){
+      $('<option value="'+forselt[i]+'">'+forselt[i]+'</option>').appendTo(forSelect);
 
-        if(forselt[i] == getvalue)
-        // var forSelect = $('<select><option>'+getvalue+'</option></select>').appendTo(itsForP);
-        $("select [value='{getvalue}']").prop('selected', true);
-      }
-      $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append(itsForP);
-      $('<button class="crossbtn" onclick="fntionForInput(this)">X</button>').appendTo(itsForP);
-
-      formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'select', 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue, 'option' : forselt});                     // push select input in form array
-      console.log(formArray);
+      if(forselt[i] == getvalue)
+      // var forSelect = $('<select><option>'+getvalue+'</option></select>').appendTo(itsForP);
+      $("select [value='{getvalue}']").prop('selected', true);
     }
-    else if(input_type == 'button')
-    {
-      $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><input type="'+input_type+'" name="'+getname+'" placeholder="'+getplc_hldr+'" class="'+getclass+'" value="'+getvalue+'"><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>'); //and it will show button if condition is true and also removing the button with cross btn 
+    $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append(itsForP);
+    $('<button class="crossbtn" onclick="fntionForInput(this)">X</button>').appendTo(itsForP);
+
+    formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'select', 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue, 'option' : forselt});                     // push select input in form array
+    console.log(formArray);
+  }
+  else if(input_type == 'button')
+  {
+    $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><input type="'+input_type+'" name="'+getname+'" placeholder="'+getplc_hldr+'" class="'+getclass+'" value="'+getvalue+'"><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>'); //and it will show button if condition is true and also removing the button with cross btn 
     
-      formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'button', 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue,});                                  // push button input in form array
-      console.log(formArray);
-    }
-    else
-    {  
-      $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><label>'+getlabel+'</label><input type="'+input_type+'" name="'+getname+'" placeholder="'+getplc_hldr+'" class="'+getclass+'" value="'+getvalue+'"><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>'); //and it will show text,number,date,email if condition is true and also removing the text,number,date,email with cross btn 
+    formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : 'button', 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue,});                                  // push button input in form array
+    console.log(formArray);
+  }
+  else
+  {  
+    $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+')').append('<p><label>'+getlabel+'</label><input type="'+input_type+'" name="'+getname+'" placeholder="'+getplc_hldr+'" class="'+getclass+'" value="'+getvalue+'"><button class="crossbtn" onclick="fntionForInput(this)">X</button></p>'); //and it will show text,number,date,email if condition is true and also removing the text,number,date,email with cross btn 
        
-      formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : input_type, 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue,});               // push text,number,date,email input in form array
-      console.log(formArray); 
-    }
+    formArray[gethead-1].subheading[getsubhead-2].form.push({ 'input' : input_type, 'label' : getlabel, 'name' : getname, 'placeholder' : getplc_hldr, 'class' : getclass, 'value' : getvalue,});               // push text,number,date,email input in form array
+    console.log(formArray); 
+  }
 
-    if($(".disable").is(":checked"))
-    {
-      var fordisable = input_type;
+  if($(".disable").is(":checked"))
+  {
+    var fordisable = input_type;
       
-      if(input_type == 'text' || input_type== 'button' || input_type == 'checkbox' || input_type == 'radio' || input_type == 'date' || input_type == 'number' || input_type == 'email')
-      {
-        fordisable = 'input';             
-      }
-      $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+') p:last-child '+fordisable).attr('disabled','disabled'); 
+    if(input_type == 'text' || input_type== 'button' || input_type == 'checkbox' || input_type == 'radio' || input_type == 'date' || input_type == 'number' || input_type == 'email')
+    {
+      fordisable = 'input';             
     }
+    $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+') p:last-child '+fordisable).attr('disabled','disabled'); 
+  }
 
-    if($(".readonly").is(":checked"))
-    {
-      var forrdonly = input_type;
+  if($(".readonly").is(":checked"))
+  {
+    var forrdonly = input_type;
       
-      if(input_type == 'text' || input_type== 'button' || input_type == 'checkbox' || input_type == 'radio' || input_type == 'date' || input_type == 'number' || input_type == 'email')
-      {
-        forrdonly = 'input'; 
-      }
-      $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+') p:last-child '+forrdonly).attr('readonly', true);
+    if(input_type == 'text' || input_type== 'button' || input_type == 'checkbox' || input_type == 'radio' || input_type == 'date' || input_type == 'number' || input_type == 'email')
+    {
+      forrdonly = 'input'; 
     }
+    $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+') p:last-child '+forrdonly).attr('readonly', true);
+  }
 
-    if($(".required").is(":checked"))
-    {
-      var forrequired = input_type;
+  if($(".required").is(":checked"))
+  {
+    var forrequired = input_type;
       
-      if(input_type == 'text' || input_type== 'button' || input_type == 'checkbox' || input_type == 'radio' || input_type == 'date' || input_type == 'number' || input_type == 'email')
-      {
-        forrequired = 'input'; 
-      }
-      $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+') p:last-child '+forrequired).attr('required', true);
+    if(input_type == 'text' || input_type== 'button' || input_type == 'checkbox' || input_type == 'radio' || input_type == 'date' || input_type == 'number' || input_type == 'email')
+    {
+      forrequired = 'input'; 
     }
+    $('main section:nth-child('+gethead+') article:nth-child('+getsubhead+') p:last-child '+forrequired).attr('required', true);
+  }
  
-    localStorage.setItem('formArray', JSON.stringify(formArray))
+  localStorage.setItem('formArray', JSON.stringify(formArray))
 
-    $(".FormAddform").trigger('reset');                              // it will remove all form data from add_form after saving the data
+  $(".FormAddform").trigger('reset');                              // it will remove all form data from add_form after saving the data
   }); 
 });
 
